7f223bba6e4974060154074f1e4e7aca7d5d655a
ebaustert675 <54185163+ebaustert675@users.noreply.github.com>
2019-09-09 15:50:07 -0500

    js commit2

--- calculator.js
+++ calculator.js
@@ -1,14 +1,28 @@
+var x = 0;
 function dis(val)
          {
+           if (val == "^")
+           {
+             let a = "^"
+             document.getElementById("calc__display").value+=a
+             x = x + "**"
+             console.log(x)
+           }
+           else
+           {
              document.getElementById("calc__display").value+=val
+             x = x + val
+             console.log(x)
+           }
          }
 
 //function that evaluates the digit and return result
 function solve()
         {
-            let x = document.getElementById("calc__display").value
             let y = eval(x)
             document.getElementById("calc__display").value = y
+            x = eval(x)
+            console.log(x)
         }
 
 //function that clear the display
@@ -16,4 +30,6 @@ function clr()
         {
             let d = " "
             document.getElementById("calc__display").value = d
+            x = 0
+            console.log(x)
         }
