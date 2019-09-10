3fa5f51ee39ab7467459b7ee7dc63b0fc31884a4
ebaustert675 <54185163+ebaustert675@users.noreply.github.com>
2019-09-10 12:39:21 -0500

    js commit3

--- calculator.js
+++ calculator.js
@@ -1,4 +1,30 @@
 var x = 0;
+function percent()
+{
+  var value = document.getElementById("calc__display").value
+  let p = value/100
+  document.getElementById("calc__display").value = p
+  x = p
+  console.log(x)
+}
+
+function del()
+{
+  var value = document.getElementById("calc__display").value
+  document.getElementById("calc__display").value = value.substr(0, value.length - 1)
+  x = x.substr(0, x.length - 1)
+  console.log(x)
+}
+
+function sqroot()
+{
+  var value = document.getElementById("calc__display").value
+  let c = Math.sqrt(value)
+  document.getElementById("calc__display").value = c
+  x = c
+  console.log(x)
+}
+
 function dis(val)
          {
            if (val == "^")
