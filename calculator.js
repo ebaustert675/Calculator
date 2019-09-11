5d3f59154342c3fd5f1130979ffa64fac2524cef
ebaustert675 <54185163+ebaustert675@users.noreply.github.com>
2019-09-11 12:00:49 -0500

    js commit4

--- calculator.js
+++ calculator.js
@@ -1,32 +1,9 @@
 var x = 0;
-function percent()
-{
-  var value = document.getElementById("calc__display").value
-  let p = value/100
-  document.getElementById("calc__display").value = p
-  x = p
-  console.log(x)
-}
 
-function del()
-{
-  var value = document.getElementById("calc__display").value
-  document.getElementById("calc__display").value = value.substr(0, value.length - 1)
-  x = x.substr(0, x.length - 1)
-  console.log(x)
-}
-
-function sqroot()
-{
-  var value = document.getElementById("calc__display").value
-  let c = Math.sqrt(value)
-  document.getElementById("calc__display").value = c
-  x = c
-  console.log(x)
-}
-
+//function that displays equations and the solutions of percent and squareroot equations
 function dis(val)
          {
+           //exponents
            if (val == "^")
            {
              let a = "^"
@@ -34,6 +11,23 @@ function dis(val)
              x = x + "**"
              console.log(x)
            }
+           //squroot
+           else if (val == "√")
+           {
+             let c = sqroot()
+             document.getElementById("calc__display").value = c
+             x = c
+             console.log(x)
+           }
+           //percent
+           else if (val == "%")
+           {
+             let p = percent()
+             document.getElementById("calc__display").value = p
+             x = p
+             console.log(x)
+           }
+           //displaying equation
            else
            {
              document.getElementById("calc__display").value+=val
@@ -42,20 +36,45 @@ function dis(val)
            }
          }
 
-//function that evaluates the digit and return result
-function solve()
-        {
-            let y = eval(x)
-            document.getElementById("calc__display").value = y
-            x = eval(x)
-            console.log(x)
-        }
+//function that deletes one number/operator from the display
+function del()
+{
+    var value = document.getElementById("calc__display").value
+    document.getElementById("calc__display").value = value.substr(0, value.length - 1)
+    x = x.substr(0, x.length - 1)
+    console.log(x)
+}
 
-//function that clear the display
+//function that finds the square root of the number
+function sqroot()
+{
+  var value = document.getElementById("calc__display").value
+  let c = Math.sqrt(value)
+  return c
+}
+
+//function that converts the number to a percent
+function percent()
+{
+  var value = document.getElementById("calc__display").value
+  let p = value/100
+  return p
+}
+
+//function that clears the display
 function clr()
         {
             let d = " "
             document.getElementById("calc__display").value = d
             x = 0
+            console.log(x)
+        }
+
+//function that evaluates the equation and displays the solution
+function solve()
+        {
+            let y = eval(x)
+            document.getElementById("calc__display").value = y
+            x = eval(x)
             console.log(x)
         }
